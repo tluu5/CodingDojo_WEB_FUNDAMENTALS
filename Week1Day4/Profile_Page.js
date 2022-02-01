237c6ccd5ace2f43977946c660a93ddf25ea99e1
tluu5 <tluu5@uic.edu>
2022-02-01 08:50:04 -0600

    Changes

--- Week1Day4/Profile_Page.js
+++ Week1Day4/Profile_Page.js
@@ -1,7 +1,7 @@
 var settingsmenu = document.querySelector(".settings-menu");
 var darkBtn = document.getElementById("dark-btn");
 function settingsMenuToggle(){
-    settingsmenu.classList.toggle(".settings-menu-height");
+    settingsmenu.classList.toggle("settings-menu-height");
 }
 darkBtn.onclick = function(){
     darkBtn.classList.toggle("dark-btn-on");
