d751709593bcbfc7afc3f865cbf942be19136758
tluu5 <tluu5@uic.edu>
2022-02-02 22:43:14 -0600

    Changes

---
+++ Week2Day2/Gaming_Dojo/Gaming_Dojo_Script.js
@@ -0,0 +1,11 @@
+function changeToPixelNinja(){
+    var banner = document.querySelector("bannerImg");
+    banner.src = "#";
+}
+function changeToStonepunk(){
+    var banner = document.querySelector("bannerImg");
+    banner.src = "#";
+}
+function buy(){
+    document.querySelector("#cartAmount").innerText++
+}
